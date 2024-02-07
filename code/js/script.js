1b6b796fa521a88aef83d0e52558b1aebcb71f43
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2024-02-06 23:41:29 -0500

    empezando a animar la pagina

---
+++ code/js/script.js
@@ -0,0 +1,23 @@
+/* const section__1 = document.querySelector('.section__1')
+const section__2 = document.querySelector('.section__2')
+const section__3 = document.querySelector('.section__3')
+const section__4 = document.querySelector('.section__4')
+const section__5 = document.querySelector('.section__5')
+const section__6 = document.querySelector('.section__6')
+const section__7 = document.querySelector('.section__7')
+const texto__completo__1 = document.querySelector('.texto__completo__1')
+const texto__completo__2 = document.querySelector('.texto__completo__2')
+const texto__completo__3 = document.querySelector('.texto__completo__3')
+const texto__completo__4 = document.querySelector('.texto__completo__4')
+const texto__completo__5 = document.querySelector('.texto__completo__5')
+const texto__completo__6 = document.querySelector('.texto__completo__6')
+const texto__completo__7 = document.querySelector('.texto__completo__7') */
+
+const sections = document.querySelectorAll('.section');
+const textosCompletos = document.querySelectorAll('.texto__completo');
+
+for (const section of sections) {
+    section.addEventListener('click', ()=>{
+        
+    })
+}
