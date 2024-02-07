97e4e83d58c76051f41747d5f9e77db99f1b7b3d
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-02-07 12:10:17 -0500

    Proyecto terminado

--- code/js/script.js
+++ code/js/script.js
@@ -15,9 +15,20 @@ const texto__completo__7 = document.querySelector('.texto__completo__7') */
 
 const sections = document.querySelectorAll('.section');
 const textosCompletos = document.querySelectorAll('.texto__completo');
+const puntoRojo = document.querySelectorAll('.punto__rojo')
 
-for (const section of sections) {
-    section.addEventListener('click', ()=>{
-        
+for (let i = 0; i < sections.length; i++) {
+    sections[i].addEventListener('click', ()=>{
+        for (let j = 0; j < textosCompletos.length; j++) {
+            if (j === i) {
+                textosCompletos[j].classList.toggle('mostrar__texto__completo');
+                sections[i].classList.add('animation__section')
+                for (let k = 0; k < puntoRojo.length; k++) {
+                    if (i === k) {
+                        puntoRojo[k].classList.add('animation__punto__rojo')
+                    }
+                }
+            }
+        }
     })
 }
